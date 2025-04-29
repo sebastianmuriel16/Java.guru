507f2a78376f434e17d3e409eb562bc9147747e9
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-04-29 01:04:07 -0500

    Update changes mvc first relation in jpa entities

---
+++ spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/entities/BeerOrder.java
@@ -0,0 +1,47 @@
+package course.spring.spring_6_rest_mvc.entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class BeerOrder {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false,nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    public boolean isNew() {return this.id == null;}
+
+    private String customerRef;
+
+    @ManyToOne
+    private Customer customer;
+
+}
