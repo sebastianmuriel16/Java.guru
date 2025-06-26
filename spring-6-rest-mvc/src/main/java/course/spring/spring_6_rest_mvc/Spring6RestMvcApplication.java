22cdf08b9997b43732ec77ba1cd6091b2d60960c
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-06-25 20:12:43 -0500

    Update changes

--- spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/Spring6RestMvcApplication.java
+++ spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/Spring6RestMvcApplication.java
@@ -2,8 +2,11 @@ package course.spring.spring_6_rest_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "course.spring.spring_6_rest_mvc")
 public class Spring6RestMvcApplication {
 
 	public static void main(String[] args) {
