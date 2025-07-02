519e136f3e2e51563f61d35ca8b7702f3c9574a4
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-07-01 22:47:49 -0500

    OpenAPI

--- spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/config/SpringSecConfig.java
+++ spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/config/SpringSecConfig.java
@@ -2,11 +2,13 @@ package course.spring.spring_6_rest_mvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
@@ -15,6 +17,7 @@ public class SpringSecConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(
                 auth -> auth
+                        .requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
         )
                 .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
