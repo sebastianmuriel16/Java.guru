606282899c6dcd5e440a909371d418186b938aab
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-07-03 19:39:55 -0500

    Actualizo submódulo spring-6-resttemplate y elimino OpenAIServiceImpl.java

--- spring-ai-introduction/src/main/java/guru/springframewrok/spring_ai_introduction/services/OpenAIServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package guru.springframewrok.spring_ai_introduction.services;
-
-public class OpenAIServiceImpl implements OpenAIService {
-    @Override
-    public String getAnswer(String question) {
-        return "";
-    }
-}
