d32be5e527f4e28bbb488084b816c9bc745d0160
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-07-04 22:35:48 -0500

    actuator-logbook

--- spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/config/SpringSecConfig.java
+++ spring-6-rest-mvc/src/main/java/course/spring/spring_6_rest_mvc/config/SpringSecConfig.java
@@ -1,8 +1,10 @@
 package course.spring.spring_6_rest_mvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -14,6 +16,20 @@ public class SpringSecConfig {
 
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+//                .securityMatcher("/actuator/**")
+//                .authorizeHttpRequests(authorize ->
+//                authorize.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(
                 auth -> auth
