06464c263142742f9a8d56b19cbd83c51d25c182
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-07-08 01:54:42 -0500

    webclient with reactive(webfluxfn) logbook

--- spring-6-webclient-webfluxfn/src/main/java/guru/spring_6_webclient/spring_6_webclient_webfluxfn/config/WebClientConfig.java
+++ spring-6-webclient-webfluxfn/src/main/java/guru/spring_6_webclient/spring_6_webclient_webfluxfn/config/WebClientConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationCodeAuthenticationTokenConverter;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.spring.webflux.LogbookExchangeFilterFunction;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
@@ -26,6 +31,10 @@ public class WebClientConfig implements WebClientCustomizer {
                 = new ServerOAuth2AuthorizedClientExchangeFilterFunction(auth2AuthorizedClientManager);
         oauth.setDefaultClientRegistrationId("springauth");
 
-        webClientBuilder.filter(oauth).baseUrl(rootUrl);
+
+
+        LogbookExchangeFilterFunction logbookWebFilter = new LogbookExchangeFilterFunction(Logbook.builder().build());
+
+        webClientBuilder.filter(oauth).filter(logbookWebFilter).baseUrl(rootUrl);
     }
 }
