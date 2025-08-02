a964719861be2388c8bb719e4c0c4fa32725f334
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-08-02 14:17:35 -0500

    move to spring-6-micro

---
+++ spring-6-micro/spring-6-cold-service/src/test/java/guru/springframework/spring_6_cold_service/listeners/DrinkPreparedListener.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring_6_cold_service.listeners;
+
+import guru.springframework.spring6restmvcapi.events.DrinkPreparedEvent;
+
+import guru.springframework.spring_6_cold_service.config.KafkaConfig;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class DrinkPreparedListener {
+
+    public AtomicInteger iceColdMessageCounter = new AtomicInteger(0);
+
+    @KafkaListener(topics = KafkaConfig.DRINK_PREPARED_TOPIC,groupId = "Cold-listener")
+    public void listen(DrinkPreparedEvent event){
+        System.out.println("I'm listening.....");
+
+        iceColdMessageCounter.incrementAndGet();
+    }
+
+}
