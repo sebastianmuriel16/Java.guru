8850850bc91c6e7a329658df65de68fc70aff1ab
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-08-02 14:02:46 -0500

    initial commit

---
+++ spring-6-icecold-service/src/main/java/guru/springframework/spring_6_icecold_service/Spring6IcecoldServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring_6_icecold_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6IcecoldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6IcecoldServiceApplication.class, args);
+	}
+
+}
