2829fd8654f9c19e2938b58073ab1010c5897957
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2025-08-02 14:01:32 -0500

    initial commit

---
+++ spring-6-cold-service/src/main/java/guru/springframework/spring_6_cold_service/Spring6ColdServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring_6_cold_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ColdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ColdServiceApplication.class, args);
+	}
+
+}
---
+++ spring-6-cold-service/src/main/java/guru/springframework/spring_6_cold_service/config/KafkaConfig.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring_6_cold_service.config;
+
+public class KafkaConfig {
+    public static final String DRINK_REQUEST_COLD_TOPIC = "drink.request.cold";
+    public static final String DRINK_PREPARED_TOPIC = "drink.prepared";
+}
